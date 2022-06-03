6b1fa75ac1cee65c7ca46a8a0d08e0c367889a9b
Tadeu Torquato <tadeu@infoconsultor.info>
2022-06-03 18:08:22 -0300

    Sem impressora

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_appcenter_bundle/flutter_appcenter_bundle.dart';
-import 'package:tpc_bluetooth_printer/tpc_bluetooth_printer.dart';
 import 'package:flutter/foundation.dart';
 import 'dart:convert';
 
@@ -49,42 +48,11 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // void initAppCenter() async {
-  //   var appSecretAndroid = "0b3130ae-6a61-4fe0-bfd2-8a3d9582f16f";
-  //   var appSecretIOS = "0b3130ae-6a61-4fe0-bfd2-8a3d9582f16f";
-  //
-  //   await AppCenter.startAsync(appSecretAndroid: appSecretAndroid, appSecretIOS: appSecretIOS);
-  // }
-
   @override
   void initState() {
-    super.initState();
-    //initAppCenter();
+    super.initState();  
 
-    AppCenter.trackEventAsync('_MyHomePageState.initState');
-    
-    //await _getImpressoraBtList();
-  }
-  
-  final List<dynamic> _blueDevices = <Map<String, dynamic>>[];  
-  
-  _getImpressoraBtList() async {
-    //setState(() => _isLoadingBt = true);
-    var result = await TpcBluetoothPrinter.listPrinters().timeout(const Duration(seconds: 5), onTimeout: () => {'success':false, 'data':'Timeout'});
-    if(result['success']){
-      for(var d in result['data']){
-        _blueDevices.add({'name':d['name'], 'address':d['address']});
-      }
-      //_atualizarImpressoras();
-    } else {
-      if(kDebugMode){print(result);}
-      //_failed = true;
-      //_reason = 'Não foi possível encontrar impressoras bluetooth';
-    }
-    //setState(() {
-      //_isLoadingBt = false;
-    //});
-
+    AppCenter.trackEventAsync('_MyHomePageState.initState');  
   }
   
   @override
